ab5f53d6579e86fb9fb423d76342af3eae8a8907
naoki0925 <naoking250912@icloud.com>
2021-03-09 12:28:15 +0000

    save:最初のページ作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  def show
+    @book = Book.new
+  end
+  
+  def index
+    @book = Book.new
+  end
+  
+  def edit
+  end 
+  
+  def update
+  end
+  
+  
+end
