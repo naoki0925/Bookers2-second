9b20342385ab007ff9068788f63015128572d5f6
naoki0925 <naoking250912@icloud.com>
2021-03-10 01:57:10 +0000

    save

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_03_09_112019) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
